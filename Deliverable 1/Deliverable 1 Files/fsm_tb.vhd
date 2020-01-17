71f54b1dd5018a34dd1bc45f7571171c75e3f3bb
Tyrone Wong <tyrone.wong@mail.mcgill.ca>
2020-01-17 13:39:57 -0500

    modified testbench

--- Deliverable 1/Deliverable 1 Files/fsm_tb.vhd
+++ Deliverable 1/Deliverable 1 Files/fsm_tb.vhd
@@ -17,7 +17,7 @@ END COMPONENT;
 --The input signals with their initial values
 SIGNAL clk, s_reset, s_output: STD_LOGIC := '0';
 SIGNAL s_input: std_logic_vector(7 downto 0) := (others => '0');
-signal test_string;
+SIGNAL test_string:string(1 to 20);
 
 CONSTANT clk_period : time := 1 ns;
 CONSTANT SLASH_CHARACTER : std_logic_vector(7 downto 0) := "00101111";
@@ -27,6 +27,7 @@ CONSTANT NEW_LINE_CHARACTER : std_logic_vector(7 downto 0) := "00001010";
 BEGIN
 dut: comments_fsm
 PORT MAP(clk, s_reset, s_input, s_output);
+
 
  --clock process
 clk_process : PROCESS
@@ -45,16 +46,45 @@ BEGIN
 	WAIT FOR 1 * clk_period;
 	ASSERT (s_output = '0') REPORT "When reading a meaningless character, the output should be '0'" SEVERITY ERROR;
 	REPORT "_______________________";
+
+--function test_string(str: string) return std_logic_vector is
+--	variable ret : std_logic_vector(a'length*8-1 downto 0);
+--begin
+--	for ind in str'range loop
+--		ret(ind*8+7 downto str*8) := std_logic_vector(to_unsigned(character'pos(str(i)),8));
+--	end loop;
+--	return ret;
+--end function string_to_std_logic_vector;
+	
+
+
+
 	
 	REPORT "___________Single Line____________";
-	s_reset <= 1;
+	s_reset <= '1';
 	WAIT FOR 1 * clk_period;
-	s_reset <= 0;
+	s_reset <= '0';
 	WAIT FOR 1 * clk_period;
 
 	REPORT "//comment\n no comment";
-	s_input <=
 
+	test_string <= "//comment\n no commen";
+
+	s_input <= SLASH_CHARACTER;
+	WAIT FOR 1 * clk_period;
+	ASSERT (s_output = '0') REPORT "When reading a SLASH_CHARACTER character, the output should be '0'" SEVERITY ERROR;
+	REPORT "_______________________";
+
+
+	s_input <= SLASH_CHARACTER;
+	WAIT FOR 1 * clk_period;
+	ASSERT (s_output = '0') REPORT "When reading a SLASH_CHARACTER character, the output should be '0'" SEVERITY ERROR;
+	REPORT "_______________________";
+
+	s_input <= std_logic_vector(to_unsigned('c',8));
+	WAIT FOR 1 * clk_period;
+	ASSERT (s_output = '1') REPORT "When reading a C character, the output should be '1'" SEVERITY ERROR;
+	REPORT "_______________________";
 
 
 	WAIT;
